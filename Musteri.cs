1f1cd35a2334f1f0d08c031a6ae87873c8ac308c
Büşra Semiz <75666551+busrasemiiz@users.noreply.github.com>
2021-01-18 18:25:13 +0300

    Create Musteri.cs

---
+++ Musteri.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetodDemo
+{
+    class Musteri
+
+    {    
+        //property (özellikler)
+        public string MusteriAdi { get; set; }
+        public int YatirimMiktari { get; set; }
+        public string Süre { get; set; }
+        public double FaizOrani { get; set; }
+
+
+
+    }
+
+   
+}
