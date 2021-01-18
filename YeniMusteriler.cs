70ee8a423aad29127807c5ea64460511d58d184c
Büşra Semiz <75666551+busrasemiiz@users.noreply.github.com>
2021-01-18 18:24:44 +0300

    Create YeniMusteriler.cs

---
+++ YeniMusteriler.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetodDemo
+{
+    class YeniMusteriler
+    {
+        public string MusteriAdi { get; set; }
+        public int YatirimMiktari { get; set; }
+        public string Süre { get; set; }
+        public double FaizOrani { get; set; }
+    }
+}
