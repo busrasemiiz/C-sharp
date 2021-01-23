f4c886a630a01863746523145abe4f17641b1ec3
Büşra Semiz <75666551+busrasemiiz@users.noreply.github.com>
2021-01-23 17:35:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> plakalar = new MyDictionary<string, int>();
+            plakalar.Add("Samsun", 55);
+            plakalar.Add("Eskişehir", 26);
+            plakalar.Add("İstanbul", 34);
+            plakalar.Add("Ankara", 06);
+
+            plakalar.Print();
+            plakalar.Clear();
+            plakalar.Add("Trabzon", 61);
+            plakalar.Print();
+        }
+    }
+}
